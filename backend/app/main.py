144a3bf3cfbd3f0d81793234516304c5365bef02
Harshi232007 <harshitharoyal232007@gmail.com>
2026-07-20 19:04:52 +0530

    Week 3 Day 2 - FastAPI connected to SQLite

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,70 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
+
+def get_connection():
+    conn = sqlite3.connect("../database/retail.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to Retail Intelligence Platform"
+        "message": "Retail Intelligence Platform API"
     }
+@app.get("/total_sales")
+def total_sales():
 
-@app.get("/about")
-def about():
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT SUM(Sales)
+        FROM sales
+    """)
+
+    total = cursor.fetchone()[0]
+
+    conn.close()
+
     return {
-        "project": "Retail Intelligence Platform",
-        "developer": "Harshitha",
-        "backend": "FastAPI"
+        "Total Sales": total
     }
+@app.get("/total_profit")
+def total_profit():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT SUM(Profit)
+        FROM sales
+    """)
+
+    total = cursor.fetchone()[0]
+
+    conn.close()
+
+    return {
+        "Total Profit": total
+    }
+@app.get("/categories")
+def categories():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT DISTINCT Category
+        FROM sales
+    """)
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return [dict(row) for row in rows]
