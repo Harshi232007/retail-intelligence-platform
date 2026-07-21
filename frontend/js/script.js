d165706b9b6c62ab369bcb2d850653e91964e170
Harshi232007 <harshitharoyal232007@gmail.com>
2026-07-21 21:03:37 +0530

    Completed Week 4 Day 4 - Added dashboard charts

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,98 @@
-console.log("Retail Intelligence Platform Loaded");
+// Total Sales
+fetch("http://127.0.0.1:8000/total_sales")
+.then(response => response.json())
+.then(data => {
+    document.getElementById("sales").innerHTML =
+    "$" + Number(data["Total Sales"]).toLocaleString();
+});
+
+// Total Profit
+fetch("http://127.0.0.1:8000/total_profit")
+.then(response => response.json())
+.then(data => {
+    document.getElementById("profit").innerHTML =
+    "$" + Number(data["Total Profit"]).toLocaleString();
+});
+
+// Total Orders
+fetch("http://127.0.0.1:8000/total_orders")
+.then(response => response.json())
+.then(data => {
+    document.getElementById("orders").innerHTML =
+    Number(data["Total Orders"]).toLocaleString();
+});
+
+// Top Products
+fetch("http://127.0.0.1:8000/top_products")
+.then(response => response.json())
+.then(products => {
+
+    let table = document.getElementById("productTable");
+
+    products.forEach(product => {
+
+        table.innerHTML += `
+        <tr>
+            <td>${product["Product.Name"]}</td>
+            <td>$${Number(product.Sales).toFixed(2)}</td>
+        </tr>
+        `;
+    });
+
+});
+fetch("http://127.0.0.1:8000/sales_by_category")
+.then(response => response.json())
+.then(data => {
+
+const labels = data.map(item => item.Category);
+
+const values = data.map(item => item.Total_Sales);
+
+new Chart(document.getElementById("salesChart"), {
+
+type: "bar",
+
+data: {
+
+labels: labels,
+
+datasets: [{
+
+label: "Sales",
+
+data: values
+
+}]
+
+}
+
+});
+
+});
+fetch("http://127.0.0.1:8000/profit_by_category")
+.then(response => response.json())
+.then(data => {
+
+const labels = data.map(item => item.Category);
+
+const values = data.map(item => item.Total_Profit);
+
+new Chart(document.getElementById("profitChart"), {
+
+type: "pie",
+
+data: {
+
+labels: labels,
+
+datasets: [{
+
+data: values
+
+}]
+
+}
+
+});
+
+});
