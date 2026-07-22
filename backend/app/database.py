6bcbaeb71fbb28e98b159c40706c2fc4e910a0b0
Harshi232007 <harshitharoyal232007@gmail.com>
2026-07-22 23:28:34 +0530

    Added requirements file for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,8 @@
 import sqlite3
+import os
 
-DATABASE_PATH = "../database/retail.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, "..", "database", "retail.db")
 
 def get_connection():
     conn = sqlite3.connect(DATABASE_PATH)
