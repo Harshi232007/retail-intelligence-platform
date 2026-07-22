21109426488b46cf55b01bb05418fbef38df5183
Harshi232007 <harshitharoyal232007@gmail.com>
2026-07-23 00:44:11 +0530

    Fix database path for Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,13 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_PATH = os.path.join(BASE_DIR, "..", "database", "retail.db")
+
+DATABASE_PATH = os.path.join(
+    BASE_DIR,
+    "..",
+    "database",
+    "retail.db"
+)
 
 def get_connection():
     conn = sqlite3.connect(DATABASE_PATH)
